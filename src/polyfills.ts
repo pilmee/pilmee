c374d53838c0788ff46436133edd9c2d47f64643
PiLMee GaTes <pilmee@MacBook-Pro-de-PiLMee.local>
2018-09-04 01:16:53 -0500

    feat(test): Change chrome launcher to PhantomJS.
    - Update script from "npm test" to "npm test --watch=false".
    - Change library "karma-chrome-launcher" to "karma-phantomjs-launcher".
    - Add intl dep.
    - Update Browser in karma.conf.js
    - Add polyfills (es6/reflect, es7/reflect, shim, intl).
    - Update Test.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -43,7 +43,9 @@
 
 /** Evergreen browsers require these. **/
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
+import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
+import 'core-js/client/shim';
 
 
 /**
@@ -78,3 +80,5 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+import 'intl';
