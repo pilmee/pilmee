ea6ebc4b6af924c633868a3fb43c257067a9494c
PiLMee GaTes <pilmee@MacBook-Pro-de-PiLMee.local>
2018-10-02 21:44:48 -0500

    fix firebase

--- src/test.ts
+++ src/test.ts
@@ -18,3 +18,7 @@ getTestBed().initTestEnvironment(
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
+
+Object.defineProperty(window, 'firebase', {
+  value: {}
+});
